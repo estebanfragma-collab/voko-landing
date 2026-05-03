fb9818d27c90c458a28b1e193d128a3bb5de180a
estebanfragma-collab <estebanfragma@gmail.com>
2026-05-03 09:15:42 -0500

    fix: redirect a app.voko.lat/onboarding

--- components/HeroForm.tsx
+++ components/HeroForm.tsx
@@ -43,7 +43,7 @@ export default function HeroForm() {
     const { data: sessionData } = await supabase.auth.getSession()
     const accessToken = sessionData.session?.access_token
     const refreshToken = sessionData.session?.refresh_token
-    window.location.href = `https://voko.lat/onboarding?access_token=${accessToken}&refresh_token=${refreshToken}`
+    window.location.href = `https://app.voko.lat/onboarding?access_token=${accessToken}&refresh_token=${refreshToken}`
   }
 
   const inputStyle: React.CSSProperties = {
