9a8c0a04560ef4edd16c88b67f02269447f6ddf7
estebanfragma-collab <estebanfragma@gmail.com>
2026-05-02 21:10:45 -0500

    chore: disable cache headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store, no-cache, must-revalidate',
+        },
+      ],
+    },
+  ],
+}
 module.exports = nextConfig
